d96f4c77ae39d93ac67c488bd6d41724f09619df
colinmd2003 <colinmd2003@gmail.com>
2023-05-24 19:09:36 -0500

    Add files via upload

--- solutions/Quadrant Selection/quadrant.cpp
+++ solutions/Quadrant Selection/quadrant.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+	int xCoordinate;
+	int yCoordinate;
+
+	cin >> xCoordinate;
+	cin >> yCoordinate;
+
+	if (xCoordinate > 0) {
+		if (yCoordinate > 0) {
+			cout << 1 << endl;
+		}
+		else {
+			cout << 4 << endl;
+		}
+	}
+
+	if (xCoordinate < 0) {
+		if (yCoordinate > 0) {
+			cout << 2 << endl;
+		}
+		else {
+			cout << 3 << endl;
+		}
+	}
+}
