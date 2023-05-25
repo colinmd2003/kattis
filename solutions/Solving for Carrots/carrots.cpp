03d801d13554c4797ecb79daa2ef17d241dffbe8
colinmd2003 <colinmd2003@gmail.com>
2023-05-24 19:14:10 -0500

    Add files via upload

--- solutions/Solving for Carrots/carrots.cpp
+++ solutions/Solving for Carrots/carrots.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+	int contestantsCount;
+	int problemsSolved;
+
+	cin >> contestantsCount >> problemsSolved;
+	string description;
+
+	for (int i = 0; i < contestantsCount; i++) {
+		cin >> description;
+	}
+	
+	cout << problemsSolved;
+}
