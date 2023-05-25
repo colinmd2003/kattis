c1b8712c1863ccf9d4dd7d4256675e47f9632407
colinmd2003 <colinmd2003@gmail.com>
2023-05-24 19:01:16 -0500

    Add files via upload

--- solutions/Breaking Branches/breakingbranches.cpp
+++ solutions/Breaking Branches/breakingbranches.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+	int stick_length;
+
+	cin >> stick_length;
+
+	if (stick_length % 2 == 0) {
+		cout << "Alice\n1" << endl;
+	}
+	else {
+		cout << "Bob" << endl;
+	}
+}
